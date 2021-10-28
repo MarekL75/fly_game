f17468260ca1e27c432d7e2ccc7afd9eae71076b
MarekL75 <marek@lecbych.cz>
2021-10-28 07:20:32 +0200

    first fly game loop

--- fly_game.py
+++ fly_game.py
@@ -1,5 +1,30 @@
-# simple pygame program
-
 import pygame
+# když se to nainportuje takto, pak staèí použít konstantu pøímo bez pygame.<konstanta>
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 pygame.init()
 
+# konstanty
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# main loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        
+        elif event.type == QUIT:
+            running = False

7428fcdb0cf61e14dfdf0cceef0f9faa2dabf78e
MarekL75 <marek@lecbych.cz>
2021-10-30 09:07:26 +0200

    buggy explosion added

--- fly_game.py
+++ fly_game.py
@@ -100,6 +100,34 @@ class Cloud(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for num in range(1, 6):
+            img = pygame.image.load(f"exp{num}.png")
+            img = pygame.transform.scale(img, (100, 100))
+            self.images.append(img)
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.counter = 0
+
+    def update(self):
+        explosion_speed = 4
+        #update explosion animation
+        self.counter += 1
+
+        if self.counter >= explosion_speed and self.index < len(self.images) - 1:
+            self.counter = 0
+            self.index += 1
+            self.image = self.images[self.index]
+
+        #if the animation is complete, reset animation index
+        if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
+            self.kill()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 timer_started = False
@@ -138,6 +166,7 @@ enemies = pygame.sprite.Group()
 clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
+explosion_group = pygame.sprite.Group()
 
 # main loop
 running = True
@@ -190,9 +219,13 @@ while running:
         move_up_sound.stop()
         move_down_sound.stop()
         collision_sound.play()
+        explosion = Explosion(player.rect.right, player.rect.bottom)
+        all_sprites.add(explosion)
+        all_sprites.draw(screen)
+        all_sprites.update()
         # If so, then remove the player and stop the loop
-        player.kill()
-        running = False
+        # player.kill()
+        # running = False
 
     if timer_started:
         passed_time = pygame.time.get_ticks() - start_time
